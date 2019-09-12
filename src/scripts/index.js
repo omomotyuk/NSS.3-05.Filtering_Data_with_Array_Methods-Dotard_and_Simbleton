763b0a1cff47cb447f3860482c0dda34e5d0ff9d
Alex <o.a.momotyuk@gmail.com>
2019-09-12 10:24:40 -0500

    forEach() method example

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,21 @@
 /*
 //
 */
+
+import businesses from "./businesses.js"
+
+// step 1
+
+const outEl = document.querySelector("#output")
+outEl.innerHTML = "<h1>Active Businesses</h1>"
+
+businesses.forEach(business => {
+  outEl.innerHTML += `
+    <h2>${business.companyName}</h2>
+    <section>
+      ${business.addressFullStreet}
+    </section>
+  `
+  outEl.innerHTML += "<hr/>"
+});
+
